f31120a9454a4a07c7b8c6b4015113d395c5001e
Andy Hawkins <andy@gently.org.uk>
2024-07-09 17:40:52 +0100

    Download drivers flags also

--- MakeAnthems.py
+++ MakeAnthems.py
@@ -2,8 +2,14 @@ import click
 import os
 import shutil
 import filecmp
+import requests
+
+from PIL import Image
 
 from RLTDatabase import RLTDatabase
+
+def findCountryCode(countries, country):
+    return list(countries.keys())[list(countries.values()).index(country)]
 
 @click.command()
 @click.option("--dest", help="Directory to contain driver anthems", required=True)
@@ -14,6 +20,8 @@ def makeAnthems(ctx, dest, src):
     debug = ctx.obj["DEBUG"]
 
     print(f"Database: '{database}', src: '{src}', dest: '{dest}'")
+
+    countries = requests.get("https://flagcdn.com/en/codes.json").json()
 
     rltDB = RLTDatabase(database)
 
@@ -29,6 +37,23 @@ def makeAnthems(ctx, dest, src):
                     print(f"Copying '{sourceFile}' to '{destFile}'")
                     shutil.copyfile(sourceFile, destFile)
             else:
-                print(f"'{driver}' - '{sourceFile}' doesn't exist")
+                print(f"'{driver['Name']}' - '{sourceFile}' doesn't exist")
+
+            destFile = os.path.join(dest, driver['Name'] + ".png")
+            if not os.path.isfile(destFile):
+                country = nationality['Name']
+                if country=="United States Of America":
+                    country="United States"
+
+                print(f"Downloading flag for '{country}' to '{destFile}'")
+
+                url = f"https://flagcdn.com/h240/{findCountryCode(countries, country)}.png"
+
+                srcImage = Image.open(requests.get(url, stream=True).raw)
+
+                destImage = Image.new("RGB", size=[srcImage.width+20, srcImage.height+20], color="white")
+                destImage.paste(srcImage, (10,10,srcImage.width+10,srcImage.height+10))
+                destImage.save(destFile, "PNG")
+                print(f"'{destFile}' - {destImage.width} x {destImage.height}")
         else:
             print(f"Couldn't find nationality for '{driver['Name']}'")
